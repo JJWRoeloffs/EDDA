16718d6ec91990afbf1e4ae3e2369190c05e413f
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-02-16 21:57:59 +0100

    Add build structure

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
 # EDDA assignments
 
-Assignment submissions for the vu course Experimental Design and Data Analisys
+Assignment submissions for the vu course Experimental Design and Data Analysis
 
-To synthesis .rmd to .pdf:
+Build to pdf with:
 
-1. Install dependencies, if not installed:
-
-install.packages("rmarkdown")
-install.packages("tinytex")
-tinytex::install_tinytex()
-tinytex:::install_prebuilt()
-
-2. Use library
-
-library(rmarkdown)
-
-3. Set working directory
-
-setwd(/your/path/here)
-
-3. Render the file!
-
-render("assign1_sol.Rmd", output_format = "pdf_document")
+```sh
+./build.sh [assignment_dir]
+```
---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install rmarkdown dependency
+R --no-restore -e 'if (!"rmarkdown" %in% installed.packages()[,"Package"]) {
+    install.packages("rmarkdown", repos = "http://cran.us.r-project.org");
+}' >/dev/null
+# Install tinytex dependency, and do so seperately because of the aditional steps
+R --no-restore -e 'if (!"tinytex" %in% installed.packages()[,"Package"]) {
+    install.packages("tinytex", repos = "http://cran.us.r-project.org");
+    tinytex::install_tinytex();
+    tinytex:::install_prebuilt();
+}' >/dev/null
+
+# wildcard expansions are guaranteed to be sorted alphabetically.
+# This means "header" coming before "part" is relevant
+cat $1/*.Rmd >>$1/$1.Rmd
+# chdir to let the data live in only one place
+#     (rstudio cd's to the .Rmd file's parent, and R -e uses current directory)
+env --chdir=$1 R --no-restore -e "rmarkdown::render('$1.Rmd', output_format = 'pdf_document')"
+rm $1/$1.Rmd
